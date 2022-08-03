5129190a975abd279f5d8ae57f74f3d81c5a3fab
Steven Chorkawy <schorkawy@gmail.com>
2022-08-03 10:28:10 -0400

    second commit

---
+++ src/webparts/claringtonPieChart/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IClaringtonPieChartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ClaringtonPieChartWebPartStrings' {
+  const strings: IClaringtonPieChartWebPartStrings;
+  export = strings;
+}
