4c16e1da0434d2327b917cbbefc285268b6686eb
Steven Chorkawy <sc13@clarington.net>
2023-02-17 09:02:11 -0500

    first commit

---
+++ src/webparts/claringtonDashboard/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IClaringtonDashboardWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ClaringtonDashboardWebPartStrings' {
+  const strings: IClaringtonDashboardWebPartStrings;
+  export = strings;
+}
